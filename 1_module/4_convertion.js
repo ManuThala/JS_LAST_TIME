1857ac7fd560131d41e5dc27516fd430009d287c
manu <manuthala7@gmail.com>
2025-06-29 12:55:01 +0530

    comparision === operator commit

--- 1_module/4_convertion.js
+++ 1_module/4_convertion.js
@@ -22,4 +22,43 @@ let booleanLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = 33;
 let stringNumber = String;
-console.log(stringNumber); // '33'
+// console.log(stringNumber); // '33'
+
+/****************** operations****************/
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue); // -3
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 ** 3); // 2^3 = 8
+// console.log(2 % 3); // 2
+
+let str1 = "hello";
+let str2 = "world";
+
+let str3 = str1 + " " + str2;
+// console.log(str3); // hello world
+
+// console.log("1" + 1); // 11
+// console.log(1 + "1"); // 11
+// console.log("1" + 1 + 1); // 111
+// console.log(1 + 1 + "1"); // 21
+
+// console.log(true + true); // 2
+// console.log(true + false); // 1
+// console.log(false + false); // 0
+// console.log(true + 1); // 2
+// console.log(false + 1); // 1
+// console.log(true + "1"); // true1
+// console.log("1" + true); // 1true
+// console.log("1" + false); // 1false
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter); // 101
+
+// study postfix and prefix in mdn docs
